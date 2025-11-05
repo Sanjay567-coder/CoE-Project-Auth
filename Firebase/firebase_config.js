582f13d1a438839b3ee07f829cfff720e0279545
Sanjay Srinivas <72558978+Sanjay567-coder@users.noreply.github.com>
2025-11-05 10:36:48 +0530

    Add files via upload

---
+++ Firebase/firebase_config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "your api key",
+  authDomain: "online-outpass.firebaseapp.com",
+  projectId: "online-outpass",
+  storageBucket: "online-outpass.firebasestorage.app",
+  messagingSenderId: "255928719269",
+  appId: "1:255928719269:web:cadbdb632a4dd82e333a4c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
